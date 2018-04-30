11cffff65b415f9b384a9e09fe8683bf1ecc3eb1
Lej77 <31554212+Lej77@users.noreply.github.com>
2018-04-30 05:39:13 +0200

    v1.0

---
+++ background.js
@@ -0,0 +1,324 @@
+
+
+async function pingTST() {
+  try {
+    await browser.runtime.sendMessage(kTST_ID, { type: 'ping' });
+  } catch (error) { return false; }
+  return true;
+}
+
+
+async function updateContextMenu() {
+  try {
+    await browser.contextMenus.removeAll();
+
+    for (let contextMenuId of [
+      'openSidebarInTab',
+      'openSidebarInWindow',
+      '---------------------',
+      'openSettings',
+    ]) {
+      let details = {
+        contexts: ['browser_action']
+      };
+      if (contextMenuId.startsWith('-')) {
+        Object.assign(details, {
+          type: 'separator',
+        });
+      } else {
+        Object.assign(details, {
+          id: contextMenuId,
+          title: browser.i18n.getMessage(`contextMenu_${contextMenuId}`),
+        });
+      }
+      await browser.contextMenus.create(details);
+    }
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+
+async function registerToTST() {
+  try {
+    await unregisterFromTST();  // Remove any style currently present.
+
+    let registrationDetails = {
+      type: 'register-self',
+      name: browser.runtime.getManifest().name,
+      listeningTypes: ['ready'],
+    };
+
+
+    // #region Style
+
+    if (settings.fixSidebarStyle) {
+
+      registrationDetails.style = `
+/* Fix sidebar page in separate tab */
+body {
+  -moz-user-select: none;
+}
+.tab {
+  box-sizing: border-box;
+}
+`;
+
+    }
+
+    // #endregion Style
+
+
+    await browser.runtime.sendMessage(kTST_ID, registrationDetails);
+  } catch (error) { return false; }
+  return true;
+}
+
+async function unregisterFromTST() {
+  try {
+    await browser.runtime.sendMessage(kTST_ID, {
+      type: 'unregister-self'
+    });
+  }
+  catch (e) {
+    // TST is not available
+    return false;
+  }
+  return true;
+}
+
+
+let windowMoveTimeoutIds = [];
+async function openTreeStyleTabSidebarInTab({ createNewWindow = false, openAfterCurrent = false, childOfCurrent = false } = {}) {
+  if (!await pingTST()) {
+    return false;
+  }
+
+  // #region Info
+
+  let internalId = settings.treeStyleTabInternalId;
+  let [activeTab,] = await browser.tabs.query({ currentWindow: true, active: true });
+  let openAfterActiveDetails = () => {
+    let details = { windowId: activeTab.windowId, index: activeTab.index + 1 };
+    if (!createNewWindow && openAfterCurrent && childOfCurrent) {
+      details.openerTabId = activeTab.id;
+    }
+    return details;
+  };
+  let tab;
+
+  // #endregion Info
+
+
+  // #region Determine Tree Style Tab's internal id
+
+  if (!internalId) {
+
+    // #region Search for open Group Tab
+
+    let allWindows = await browser.windows.getAll();
+
+    for (let window of allWindows) {
+      if (internalId) {
+        break;
+      }
+      try {
+        let tstTabs = await getTabsFromTST(window.id, true);
+        for (let tstTab of tstTabs) {
+          if (tstTab.states.includes('group-tab')) {
+            let groupURLInfo = getGroupTabInfo(tstTab.url);
+            if (groupURLInfo) {
+              internalId = groupURLInfo.internalId;
+              break;
+            }
+          }
+        }
+      } catch (error) { }
+    }
+
+    // #endregion Search for open Group Tab
+
+
+    // #region Open a new Group Tab
+
+    if (!internalId) {
+      let tempTab;
+      try {
+        tempTab = await browser.tabs.create(Object.assign(openAfterActiveDetails(), { active: false }));
+        try {
+          let groupTab = await browser.runtime.sendMessage(kTST_ID, {
+            type: 'group-tabs',
+            tabs: [tempTab.id]
+          });
+          let groupURLInfo = getGroupTabInfo(groupTab.url);
+          if (groupURLInfo) {
+            internalId = groupURLInfo.internalId;
+          }
+        } catch (error) { }
+      } finally {
+        if (tempTab) {
+          await browser.tabs.remove(tempTab.id);
+        }
+      }
+    }
+
+    // #endregion Open a new Group Tab
+
+
+    if (!internalId) {
+      return null;
+    }
+    browser.storage.local.set({ treeStyleTabInternalId: internalId });
+    settings.treeStyleTabInternalId = internalId;
+  }
+
+  // #endregion Determine Tree Style Tab's internal id
+
+
+  // #region Open Sidebar Page
+
+  let sidebarTab;
+  let sidebarURL = getSidebarURL(internalId);
+
+  let promiseInfo;
+  try {
+    promiseInfo = await getPromiseWithResolve();
+
+    // Listener for loading complete:
+    let loadListener = (tabId, changeInfo, tab) => {
+      if (!sidebarTab || sidebarTab.id !== tabId) {
+        return;
+      }
+      if (changeInfo.status === 'complete') {
+        promiseInfo.resolve(changeInfo.url);
+      }
+    };
+    try {
+      browser.tabs.onUpdated.addListener(loadListener);
+
+      // Open Sidebar Page:
+      if (tab) {
+        await browser.tabs.update(tab.id, { url: sidebarURL });
+      } else {
+        let createDetails = { url: sidebarURL, active: !createNewWindow };
+
+        if (createNewWindow || openAfterCurrent) {
+          createDetails = Object.assign(openAfterActiveDetails(), createDetails);
+        } else {
+          createDetails = Object.assign({ windowId: activeTab.windowId }, createDetails);
+        }
+
+        tab = await browser.tabs.create(createDetails);
+      }
+
+      // Wait for sidebar tab to load:
+      sidebarTab = tab;
+      internalId = await Promise.race([delay(3000), promiseInfo.promise]);
+    } finally {
+      browser.tabs.onUpdated.removeListener(loadListener);
+    }
+  } finally {
+    if (promiseInfo) {
+      promiseInfo.resolve(false);
+    }
+  }
+
+  // #endregion Open Sidebar Page
+
+
+  // #region Move to new window
+
+  if (createNewWindow) {
+    let moveToNewWindow = () => browser.windows.create({ incognito: activeTab.incognito, tabId: tab.id, });
+    if (settings.delayBeforeWindowSeperationInMilliseconds && settings.delayBeforeWindowSeperationInMilliseconds > 0) {
+      let timeoutId = setTimeout(() => {
+        windowMoveTimeoutIds = windowMoveTimeoutIds.filter(aId => aId !== timeoutId);
+        moveToNewWindow();
+      }, settings.delayBeforeWindowSeperationInMilliseconds);
+      windowMoveTimeoutIds.push(timeoutId);
+    } else {
+      await moveToNewWindow();
+    }
+  }
+
+  // #endregion Move to new window
+
+
+  return tab;
+}
+
+
+function getDefaultMoveDetails(overrides = {}) {
+  return Object.assign({
+    openAfterCurrent: settings.openAfterCurrentTab,
+    childOfCurrent: settings.openAsChildOfCurrentTab,
+  }, overrides);
+}
+
+
+settingsLoaded.finally(async () => {
+
+  // #region Settings
+
+  handleSettingChanges = (changes, areaName) => {
+    if (changes.fixSidebarStyle) {
+      registerToTST();
+    }
+    if (changes.delayBeforeWindowSeperationInMilliseconds) {
+      for (let id of windowMoveTimeoutIds) {
+        clearTimeout(id);
+      }
+      windowMoveTimeoutIds = [];
+    }
+  };
+
+  // #endregion Settings
+
+
+  // #region Tree Stlye Tab
+
+  browser.runtime.onMessageExternal.addListener((aMessage, aSender) => {
+    if (aSender.id !== kTST_ID) {
+      return;
+    }
+    switch (aMessage.type) {
+      case 'ready': {
+        // passive registration for secondary (or after) startup:
+        registerToTST();
+        return Promise.resolve(true);
+      } break;
+    }
+  });
+  if (!registerToTST()) {
+    setTimeout(registerToTST, 5000);
+  }
+
+  // #endregion Tree Stlye Tab
+
+
+  // #region Context Menu
+
+  updateContextMenu();
+  browser.contextMenus.onClicked.addListener((info, tab) => {
+
+    switch (info.menuItemId) {
+      case 'openSettings': {
+        browser.runtime.openOptionsPage();
+      } break;
+
+      case 'openSidebarInTab': {
+        openTreeStyleTabSidebarInTab(getDefaultMoveDetails({ createNewWindow: false }));
+      } break;
+      case 'openSidebarInWindow': {
+        openTreeStyleTabSidebarInTab(getDefaultMoveDetails({ createNewWindow: true }));
+      } break;
+    }
+  });
+  browser.browserAction.onClicked.addListener((tab) => {
+    openTreeStyleTabSidebarInTab(getDefaultMoveDetails({ createNewWindow: settings.browserAction_OpenInNewWindow }));
+  });
+
+  // #endregion Context Menu
+
+});
---
+++ common.js
@@ -0,0 +1,212 @@
+
+const kTST_ID = 'treestyletab@piro.sakura.ne.jp';
+
+
+async function delay(timeInMilliseconds) {
+  return await new Promise((resolve, reject) => timeInMilliseconds < 0 ? resolve() : setTimeout(resolve, timeInMilliseconds));
+}
+
+async function getPromiseWithResolve() {
+  let aPromise;
+  let aReject;
+  let aResolve;
+  await new Promise((resolve, reject) => {
+    aPromise = new Promise((resolvePromise, rejectPromise) => {
+      aResolve = resolvePromise;
+      aReject = rejectPromise;
+      resolve();
+    });
+  });
+  return {
+    promise: aPromise,
+
+    resolve: aResolve,
+    reject: aReject,
+  };
+}
+
+
+// #region Tree Style Tab
+
+function getSidebarURL(internalId) {
+  return 'moz-extension://' + internalId + '/sidebar/sidebar.html';
+}
+
+function getGroupTabURL(name = null, temporary = undefined, internalId = null) {
+  let url = internalId ? 'moz-extension://' + internalId + '/resources/group-tab.html' : 'about:treestyletab-group';
+  let firstArg = true;
+  let prepareForArg = () => {
+    url += firstArg ? '?' : '&';
+    firstArg = false;
+  };
+  if (name && typeof name === 'string') {
+    prepareForArg();
+    url += 'title=' + encodeURIComponent(name);
+  }
+  if (temporary !== undefined) {
+    prepareForArg();
+    url += 'temporary=' + (temporary ? 'true' : 'false');
+  }
+  return url;
+}
+
+function getGroupTabInfo(url) {
+  if (!url || typeof url !== 'string') {
+    return null;
+  }
+  let removeLength = (string, removeLength) => {
+    return string.length <= removeLength ? '' : string.substr(removeLength);
+  };
+
+  let internalId = null;
+
+  let legacyURI = 'about:treestyletab-group';
+  if (url.startsWith(legacyURI)) {
+    url = removeLength(url, legacyURI.length);
+  } else {
+    let start = 'moz-extension://';
+    if (!url.startsWith(start)) {
+      return null;
+    }
+    url = removeLength(url, start.length);
+
+    let separatorIndex = url.indexOf('/');
+    if (separatorIndex < 0) {
+      return null;
+    }
+    internalId = url.substr(0, separatorIndex);
+    url = removeLength(url, separatorIndex + 1);
+
+    let location = 'resources/group-tab.html';
+    if (!url.startsWith(location)) {
+      return null;
+    }
+    url = removeLength(url, location.length);
+  }
+
+  let info = {};
+
+  if (internalId) {
+    info.internalId = internalId;
+  }
+
+  if (url.startsWith('?')) {
+    url = removeLength(url, 1);
+
+    let getInfo = (arg, id, key, handleValue) => {
+      if (arg.startsWith(id)) {
+        if (!Object.keys(info).includes(key)) {
+          let value = removeLength(arg, id.length);
+          if (handleValue && typeof handleValue === 'function') {
+            value = handleValue(value);
+          }
+          info[key] = value;
+        }
+        return true;
+      } else {
+        return false;
+      }
+    };
+    let tests = [
+      (arg) => {
+        return getInfo(arg, 'title=', 'name', (value) => {
+          return decodeURIComponent(value);
+        });
+      },
+      (arg) => {
+        return getInfo(arg, 'temporary=', 'temporary', (value) => {
+          value = value.toLowerCase().trim();
+          return value === 'true';
+        });
+      },
+    ];
+    for (let arg of url.split('&')) {
+      for (let test of tests) {
+        if (test(arg)) {
+          break;
+        }
+      }
+    }
+  }
+
+  return Object.assign({
+    name: 'Group',
+    temporary: false,
+  }, info);
+}
+
+async function getTabsFromTST(windowId, flatArray = false) {
+  // Flat array: each tab is in the original array. If the array isn't flat then only root tabs occur in the array and the other tabs are only accessible through the tabs children property.
+  let message = {
+    type: 'get-tree',
+    window: windowId,
+  };
+  if (flatArray) {
+    message.tabs = '*';
+  }
+  return await browser.runtime.sendMessage(kTST_ID, message);
+}
+// #endregion Tree Style Tab
+
+
+// #region Settings
+
+function getDefaultSettings() {
+  return {
+    treeStyleTabInternalId: null,
+
+    
+    fixSidebarStyle: true,
+    
+    browserAction_OpenInNewWindow: true,
+
+    openAfterCurrentTab: true,
+    openAsChildOfCurrentTab: false,
+    delayBeforeWindowSeperationInMilliseconds: 500,
+  };
+}
+
+
+var settings = getDefaultSettings();
+let changed = {};
+function applySettingChanges(target, changes, fallbackToDefault = true) {
+  let defaultSettings;
+  let defaultSettingsKeys;
+
+  for (let key of Object.keys(changes)) {
+    if (Object.keys(changes[key]).includes('newValue')) {
+      target[key] = changes[key].newValue;
+    } else {
+      if (fallbackToDefault && !defaultSettings) {
+        defaultSettings = getDefaultSettings();
+        defaultSettingsKeys = Object.keys(defaultSettings);
+      }
+      if (fallbackToDefault && defaultSettingsKeys.includes(key)) {
+        target[key] = defaultSettings[key];
+      } else {
+        delete target[key];
+      }
+    }
+  }
+}
+var handleSettingChanges;
+browser.storage.onChanged.addListener((changes, areaName) => {
+  applySettingChanges(settings, changes);
+  if (changed) {
+    applySettingChanges(changed, changes);
+  }
+  if (handleSettingChanges) {
+    handleSettingChanges(changes, areaName);
+  }
+});
+let settingsLoaded = browser.storage.local.get(null).then((value) => {
+  let changedKeys = Object.keys(changed);
+  for (let key of Object.keys(value)) {
+    if (!changedKeys.includes(key)) {
+      settings[key] = value[key];
+    }
+  }
+  changed = null;
+});
+
+// #endregion Settings
